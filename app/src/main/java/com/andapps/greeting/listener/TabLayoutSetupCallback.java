9e5cf9d157515388e34600d1d17c3f32821f166d
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-10-27 18:08:12 +0530

    Create TabLayoutSetupCallback.java

---
+++ app/src/main/java/com/andapps/greeting/listener/TabLayoutSetupCallback.java
@@ -0,0 +1,9 @@
+package com.andapps.greeting.listener;
+
+import android.support.v4.view.ViewPager;
+
+
+public interface TabLayoutSetupCallback {
+
+    void setupTabLayout(ViewPager viewPager);
+}
