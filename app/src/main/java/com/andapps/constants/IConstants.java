e58ae1a5454c9c422c2edffe190961a0c43afe2d
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-10-27 18:04:32 +0530

    Delete IConstants.java

--- app/src/main/java/com/andapps/constants/IConstants.java
+++
@@ -1,9 +0,0 @@
-package com.andapps.greeting.constants;
-
-public interface IConstants {
-
-    String FROM = "from";
-    // tags used to attach the fragments
-     String TAG_DIWALI = "diwali";
-     String TAG_NEW_YEAR = "newYear";
-}
