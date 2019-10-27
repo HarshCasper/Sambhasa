452929f3424f540c164d7a7b7c895069257ad50e
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-10-27 18:05:19 +0530

    Create IConstants.java

---
+++ app/src/main/java/com/andapps/greeting/constants/IConstants.java
@@ -0,0 +1,10 @@
+package com.andapps.greeting.constants;
+
+
+public interface IConstants {
+
+    String FROM = "from";
+    // tags used to attach the fragments
+     String TAG_DIWALI = "diwali";
+     String TAG_NEW_YEAR = "newYear";
+}
